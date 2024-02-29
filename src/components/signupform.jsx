a9870ab4874e6d74de8d0161209a3ca0704b104d
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-29 00:21:31 +0000

    signupform initial set up done

--- src/components/signupform.jsx
+++ src/components/signupform.jsx
@@ -4,6 +4,7 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function SignUpForm() {
   const [validated, setValidated] = useState(false);
@@ -22,63 +23,69 @@ function SignUpForm() {
     <Form noValidate validated={validated} onSubmit={handleSubmit}>
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
+          <Form.Label>Name</Form.Label>
           <Form.Control
             required
             type="text"
-            placeholder="First name"
+            placeholder="name"
             defaultValue="Mark"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Last name</Form.Label>
+          <Form.Label>Age</Form.Label>
           <Form.Control
             required
             type="text"
-            placeholder="Last name"
+            placeholder="Age"
             defaultValue="Otto"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>Username</Form.Label>
+          <Form.Label>Height</Form.Label>
           <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+           
             <Form.Control
               type="text"
-              placeholder="Username"
+              placeholder="Height"
               aria-describedby="inputGroupPrepend"
               required
             />
             <Form.Control.Feedback type="invalid">
-              Please choose a username.
+              Please input Height.
             </Form.Control.Feedback>
           </InputGroup>
         </Form.Group>
       </Row>
       <Row className="mb-3">
         <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" required />
+          <Form.Label>Weight</Form.Label>
+          <Form.Control type="text" placeholder="Weight" required />
           <Form.Control.Feedback type="invalid">
-            Please provide a valid city.
+            Please input Weight.
           </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom04">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid state.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom05">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control type="text" placeholder="Zip" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid zip.
-          </Form.Control.Feedback>
-        </Form.Group>
+{/* dropdown for goals */}
+<Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Goals
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Back</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Cardio</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Chest</Dropdown.Item>
+        <Dropdown.Item href="#/action-1">Lower Arms</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Lower Legs</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Neck</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Shoulder</Dropdown.Item>
+        <Dropdown.Item href="#/action-1">Upper Arms</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Upper Legs</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Waist</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+     
       </Row>
       <Form.Group className="mb-3">
         <Form.Check
