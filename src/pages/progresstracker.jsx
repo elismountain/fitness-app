11924686b705faaa3a48b0e6d0ffd41a153f6bff
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-03-03 00:38:51 +0000

    duration of exercise now appending on the page, clear all data button doesnot empty out the progress tracker

--- src/pages/progresstracker.jsx
+++ src/pages/progresstracker.jsx
@@ -5,7 +5,9 @@ const ProgressTracker = () => {
 
   let mood = localStorage.getItem("emoji");
   let goalachived = localStorage.getItem("goal");
-  let durationofexercise = localStorage.getItem("time")
+  // let durationofexercise = JSON.parse(localStorage.getItem("time"))
+  let durationofexercise = localStorage.getItem('timerDurations');
+  let timer = JSON.parse(durationofexercise);
   let waterintake = localStorage.getItem("waterIntake")
 
 
@@ -17,7 +19,7 @@ const ProgressTracker = () => {
         <p className="description-text">Mood : {mood} </p>
         <p className="description-text">Water Intake : {waterintake}</p>
         <p className="description-text">Goals achieved : {goalachived} </p>
-        <p className="description-text">Duration of Exercise : {durationofexercise} </p>
+        <p className="description-text">Duration of Exercise : {timer} </p>
       </div>
       <button onClick={()=> localStorage.clear()} >CLEAR ALL DATA</button>
 
