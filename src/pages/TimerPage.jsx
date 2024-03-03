6dae3e59de06482aa7480c6e724c404c5eb00078
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-03-03 15:24:39 +0000

    timer page styling done, checked mobile responsiveness- working

--- src/pages/TimerPage.jsx
+++ src/pages/TimerPage.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Timer from '../components/Timer';
-import './TimerPage.css';
+import React from "react";
+import Timer from "../components/Timer";
+import "./TimerPage.css";
 import { useLocation } from "react-router-dom";
 
 const TimerPage = () => {
@@ -10,39 +10,35 @@ const TimerPage = () => {
 
   return (
     <div className="timer-block">
-      
       <div className="description">
-        <h2 className='description-header'>
-          {exercise.name}
-          </h2>
-        <p className="description-text">
-          {exercise.instructions}
-          </p>
+        <h2 className="description-header">{exercise.name}</h2>
+        <p className="description-text">{exercise.instructions}</p>
       </div>
       <div className="video-container">
-      <h2 className='description-header'>
-          Warm Up
-          </h2>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/qZkxKTxXY8M?si=sDBmxv_V6acpbY-i" 
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-    </div>
-      <div className="demonstration">
-        <h2 className='demonstration-header'>Video/GIF Demonstration</h2>
-        <iframe src={exercise.gifUrl} width="560" height="315">
-        </iframe>
+        <h2 className="description-header">Warm Up</h2>
+        <div className="iframe-wrapper">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/qZkxKTxXY8M?si=sDBmxv_V6acpbY-i"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
-      <div className='timer-wrap'>
+      <div className="demonstration">
+        <h2 className="demonstration-header">Video/GIF Demonstration</h2>
+        <div className="iframe-wrapper">
+          <iframe src={exercise.gifUrl} width="560" height="315"></iframe>
+          </div>
+        
+      </div>
+      <div className="timer-wrap">
         <Timer />
       </div>
     </div>
   );
-}
+};
 
 export default TimerPage;
-
