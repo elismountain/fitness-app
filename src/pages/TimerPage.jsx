90caa2252edc5345110d75393ea3306aa734046a
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-03-03 14:38:01 +0000

    warm up video added to TimerPage and is now displaying on the app

--- src/pages/TimerPage.jsx
+++ src/pages/TimerPage.jsx
@@ -10,7 +10,16 @@ const TimerPage = () => {
 
   return (
     <div className="timer-block">
-
+        <div className="video-container">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/qZkxKTxXY8M?si=sDBmxv_V6acpbY-i" 
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
       <div className="description">
         <h2 className='description-header'>
           {exercise.name}
