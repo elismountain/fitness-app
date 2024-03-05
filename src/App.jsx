0d56d61b2dc13f3e6523b8a9f51dd338ce998b3e
AndyFt <149973636+AndyFt@users.noreply.github.com>
2024-03-05 20:46:00 +0000

    fixed ProgressTracker path on Add.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
 import MyNavbar from './components/MyNavbar';
 import Footer from './components/Footer';
-import ProgressTracker from './pages/progresstracker';
+import ProgressTracker from './pages/ProgressTracker';
 import WaterTracker from './components/watertracker'; // Import WaterTracker component
 import Contact from './pages/contact';
 import WorkoutPlan from './pages/WorkoutPlan';
